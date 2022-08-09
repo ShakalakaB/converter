c110ddc58243168239a3798ebab6ec71e2593a42
aldora <aldora988@gmail.com>
2022-08-09 11:48:19 +0800

    Add google analytics

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,28 @@
 import type { NextPage } from "next";
+import Script from "next/script";
 import Head from "next/head";
 import { SqlParser } from "../components/SqlParser";
 
 const Home: NextPage = () => {
   return (
     <>
+      {/*Google tag (gtag.js)*/}
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+      />
+
+      <Script id="google-analytics" strategy="lazyOnload">
+        {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+              page_path: window.location.pathname,
+            });
+                `}
+      </Script>
+
       <Head>
         <title>Convertor | Online convertor tools</title>
         <meta name="description" content="Online convertor tools" />
