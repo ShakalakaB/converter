6cad3c2cd91f8995df7d3779ea02842420da96ee
aldora <aldora988@gmail.com>
2022-08-09 14:07:15 +0800

    Add meta tag

--- pages/index.tsx
+++ pages/index.tsx
@@ -25,7 +25,38 @@ const Home: NextPage = () => {
 
       <Head>
         <title>Convertor | Online convertor tools</title>
-        <meta name="description" content="Online convertor tools" />
+        {/*<meta name="description" content="Online convertor tools" />*/}
+        <meta
+          name="description"
+          content="Generate class from database table online, convert a 'CREATE TABLE' script from MS-SQL, ORACLE, MYSQL,POSTGRESQL, SQLite database to class in C#, TypeScript, VB.NET, JAVA, PHP, JavaScript, Python and more programming languages."
+        />
+        <meta
+          name="keywords"
+          content="sql table to class, SQL Entity Class Generator, sql table to c# class, sql table to javascript class, sql table to python class, sql table to java class sql table to vb.net class, online class generator from sql server,postgresql to class"
+        />
+        <meta
+          name="og:title"
+          content="Convertor | SQL Table to Entity Class Generator "
+        />
+        <meta
+          name="og.description"
+          content="Generate class from database table online, convert a 'CREATE TABLE' script from MS-SQL, ORACLE, MYSQL,POSTGRESQL, SQLite database to class in C#, TypeScript, VB.NET, JAVA, PHP, JavaScript, Python and more programming languages."
+        />
+        <meta
+          name="og.image"
+          content="https://convertor.aldoraweb.com/logo.png"
+        />
+        <meta name="og.url" content="https://convertor.aldoraweb.com" />
+        <meta name="og.site_name" content="convertor.aldoraweb.com" />
+        <meta name="twitter:url" content="https://convertor.aldoraweb.com" />
+        <meta
+          name="twitter:image"
+          content="https://convertor.aldoraweb.com/logo.png"
+        />
+        <meta name="twitter:label1" content="Written by" />
+        <meta name="twitter:data1" content="1354" />
+        <meta name="og:image:width" content="Aldora Lee" />
+        <meta name="og:image:height" content="1076" />
       </Head>
       <SqlParser />
     </>
